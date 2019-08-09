e8221f571d1f4e58425f5b2c420284136ca18de9
frankie <r.rudchenko@nton.info>
2019-08-09 04:28:19 +0300

    gatsby remark

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,18 +2,19 @@ module.exports = {
   siteMetadata: {
     title: `pozitiff gatsby netlify cms`,
     description: `pozitiff gatsby netlify cms`,
-    author: `@gatsbyjs`,
+    author: `@gatsbyjs`
   },
   plugins: [
     `gatsby-plugin-netlify-cms`,
+    `gatsby-transformer-remark`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `img`,
-        path: `${__dirname}/static`,
-      },
+        path: `${__dirname}/static`
+      }
     },
     `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-  ],
+    `gatsby-plugin-sharp`
+  ]
 }
---
+++ src/cms/cms.js
@@ -0,0 +1,7 @@
+// import AboutPagePreview from './preview-templates/AboutPagePreview'
+// import BlogPostPreview from './preview-templates/BlogPostPreview'
+// import RoomsPagePreview from './preview-templates/RoomsPagePreview';
+// import IndexPagePreview from './preview-templates/IndexPagePreview';
+
+// CMS.registerPreviewTemplate('index', IndexPagePreview)
+// CMS.registerPreviewTemplate('rooms', RoomsPagePreview)
