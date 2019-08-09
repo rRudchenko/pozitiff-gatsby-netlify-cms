b00131400c808d63f9ff944b4f30f09310f1646b
frankie <r.rudchenko@nton.info>
2019-08-09 05:04:26 +0300

    first content from cms

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,42 @@
 import React from 'react'
-// import { Link } from 'gatsby'
+import { graphql } from 'gatsby'
+import Img from 'gatsby-image'
 
-const IndexPage = ({ data }) => (
-  <section>
-    <div className='container'>
-      <h1>Hi people</h1>
-      <p>Welcome to your new Gatsby site.</p>
-      <p>Now go build something great.</p>
-    </div>
-  </section>
-)
+const IndexPage = ({ data }) => {
+  console.log('DATA', data.allImageSharp.edges[0].node.fluid.originalImg)
+  return (
+    <section>
+      <div className='container'>
+        <h1>Hi people</h1>
+        <p>Welcome to your new Gatsby site.</p>
+        <p>Now go build something great.</p>
+      </div>
+      <p>{data.allImageSharp.edges[0].node.fluid.originalName}</p>
+      <p>{data.allImageSharp.edges[0].node.fluid.presentationWidth}</p>
+      <p>{data.allImageSharp.edges[0].node.fluid.presentationHeight}</p>
+      {/* <Img src={data.allImageSharp.edges[0].node.fluid.originalImg} /> */}
+    </section>
+  )
+}
 
 export default IndexPage
+
+export const pageQuery = graphql`
+  query IndexPage {
+    allImageSharp {
+      edges {
+        node {
+          fluid {
+            originalName
+            originalImg
+            presentationWidth
+            presentationHeight
+          }
+        }
+      }
+    }
+  }
+`
 
 // {/* <div>
 //         <h2>{name}</h2>
