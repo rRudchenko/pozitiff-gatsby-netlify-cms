7a7b3509ff0f84a61a8e1c89a65aa1aa03d27da2
frankie <r.rudchenko@nton.info>
2019-08-09 03:34:49 +0300

    gatsby structure, netlify cms

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,25 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react'
+// import { Link } from 'gatsby'
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
-
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+const IndexPage = ({ data }) => (
+  <section>
+    <div className='container'>
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+  </section>
 )
 
 export default IndexPage
+
+// {/* <div>
+//         <h2>{name}</h2>
+//       </div>
+//       <div>
+//         <p>{description}</p>
+//       </div>
+//       <div>
+//         <p>{price}</p>
+//       </div>
+//       <img src={image} /> */}
